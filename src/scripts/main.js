457a1f1f63d0d00ef0b4a3f4c46dd9371f8f9188
Cieliocas <francielioevangelista@hotmail.com>
2024-11-20 04:13:32 -0300

    Contador de dias

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date()
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
+    
+
+
+
+}, 1000);
+
+
+
+
+// 12/12/2024 às 19hrs
